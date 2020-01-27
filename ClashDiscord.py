36667cb618dc9ee24ce7677e0d4aead68cb9fe84
Westo48 <46290484+Westo48@users.noreply.github.com>
2020-01-26 19:00:45 -0600

    initial commit of troop donation

--- ClashDiscord.py
+++ ClashDiscord.py
@@ -5,6 +5,7 @@ from Clash_Responder import *
 RazgrizTag = 'RGQ8RGU9'
 TheMightyHeroesTag = 'JJRJGVR0'
 
+
 client = commands.Bot(command_prefix='!')
 
 
@@ -12,7 +13,7 @@ client = commands.Bot(command_prefix='!')
 async def on_ready():
     print('RazBot is ready')
 
-
+# todo add methods to this command
 @client.command()
 async def helpme(ctx):
     await ctx.send(f'Here are some helpful functions: !ping, !hellothere, !trooplvl, !waroverview, !scoreboard, !war, !wartime, !noattack, !noatk')
@@ -28,7 +29,7 @@ async def ping(ctx):
     await ctx.send(f'pong {round(client.latency) * 1000}ms')
 
 
-@client.command(description='enter your player name and hero/troop/spell you are looking for')
+@client.command(description='Enter your player name and hero/troop/spell you are looking for. If your name has a space in it replace that space with -')
 async def trooplvl(ctx, player, *, troop):
     await ctx.send(f'{user_troop_levels(player, troop)}')
 
@@ -37,6 +38,16 @@ async def trooplvl(ctx, player, *, troop):
 async def alltrooplvl(ctx, player):
     for message_line in all_user_levels(player):
         await ctx.send(f'{message_line}')
+
+
+@client.command(desctiption='enter your player name and troop to see if you will be able to donate specified troop', aliases=['donation', 'donate'])
+async def donationchecker(ctx, player, *, troop):
+    await ctx.send(f'{donation_checker(player, troop)}')
+
+
+@client.command(description='enter the TH you are looking for and the amount of gold/elixir in TH or one storage')
+async def warweight(ctx, THlvl, *, amount):
+    await ctx.send(f'{war_weight(THlvl, amount)}')
 
 
 @client.command(aliases=['war', 'scoreboard'])
@@ -49,7 +60,7 @@ async def wartime(ctx):
     await ctx.send(f'{curr_war_time(TheMightyHeroesTag)}')
 
 # todo make war members call
-@client.command(aliases=['warmembers', 'warmember', 'warmem', 'warmemstar', 'warmemstars'],description='overview of all members in war', hidden=True)
+@client.command(aliases=['warmembers', 'warmember', 'warmem', 'warmemstar', 'warmemstars'], description='overview of all members in war', hidden=True)
 async def warmemberstars(ctx):
     for message_line in all_attack_stars(TheMightyHeroesTag):
         await ctx.send(f'{message_line}')
@@ -60,18 +71,20 @@ async def noattack(ctx):
     await ctx.send(f'{curr_no_atk(TheMightyHeroesTag)}')
 
 
-@client.command(aliases=['allatk'],description='showing all attacks for every member', hidden=True)
+@client.command(aliases=['allatk'], description='showing all attacks for every member', hidden=True)
 async def allattacks(ctx):
     for message_line in all_attacks(TheMightyHeroesTag):
         await ctx.send(f'{message_line}')
 
 
-@client.command(aliases=['cwltime'],description='shows time remaining in the current clan war league')
+# cwl war
+
+@client.command(aliases=['cwltime'], description='shows time remaining in the current clan war league')
 async def cwlwartime(ctx):
     await ctx.send(f'{cwl_war_time(TheMightyHeroesTag)}')
 
 
-@client.command(aliases=['cwlwar'],description='shows the overview of the current war')
+@client.command(aliases=['cwlwar'], description='shows the overview of the current war league')
 async def cwlwaroverview(ctx):
     await ctx.send(f'{cwl_curr_war_overview(TheMightyHeroesTag)}')
 
