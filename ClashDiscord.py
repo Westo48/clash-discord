bb82abe5cf103595d7eb61b9f2889446bd3a68cd
Westo48 <westonsouthers19@gmail.com>
2021-12-03 20:32:33 -0600

    find clan users command

--- ClashDiscord.py
+++ ClashDiscord.py
@@ -574,7 +574,7 @@ async def activesupertroop(ctx):
 @client.command(
     aliases=['finduserfromplayer'],
     brief='player',
-    description='returns the user that claimed a specified player'
+    description='returns the user linked to a requested player'
 )
 async def finduser(ctx, player_tag):
     async with ctx.typing():
@@ -586,8 +586,8 @@ async def finduser(ctx, player_tag):
         await ctx.send(f"could not find player with tag {player_tag}")
         return
 
-    field_dict_list = discord_responder.find_user_from_tag(
-        player_obj, ctx.guild.members)
+    field_dict_list = [discord_responder.find_user_from_tag(
+        player_obj, ctx.guild.members)]
 
     embed_list = discord_responder.embed_message(
         Embed=discord.Embed,
@@ -990,6 +990,66 @@ async def supertroopsearch(ctx, *, unit_name):
         bot_prefix=ctx.prefix,
         bot_user_name=ctx.bot.user.name,
         thumbnail=clan_obj.clan_icons,
+        field_list=field_dict_list,
+        image_url=None,
+        author_display_name=ctx.author.display_name,
+        author_avatar_url=(ctx.author.avatar_url.BASE +
+                           ctx.author.avatar_url._url)
+    )
+    for embed in embed_list:
+        await ctx.send(embed=embed)
+
+
+@client.command(
+    aliases=['findclanuser', 'findclanmemberusers'],
+    brief='clan',
+    description="returns the users linked to the active player's clan"
+)
+async def findclanusers(ctx):
+    async with ctx.typing():
+        db_player_obj = db_responder.read_player_active(
+            ctx.author.id)
+
+    verification_payload = discord_responder.clan_verification(
+        db_player_obj, ctx.author, razbot_data.header)
+    if not verification_payload['verified']:
+        embed_list = discord_responder.embed_message(
+            Embed=discord.Embed,
+            color=discord.Color(client_data.embed_color),
+            icon_url=(ctx.bot.user.avatar_url.BASE +
+                      ctx.bot.user.avatar_url._url),
+            title=None,
+            bot_prefix=ctx.prefix,
+            bot_user_name=ctx.bot.user.name,
+            thumbnail=None,
+            field_list=verification_payload['field_dict_list'],
+            image_url=None,
+            author_display_name=ctx.author.display_name,
+            author_avatar_url=(ctx.author.avatar_url.BASE +
+                               ctx.author.avatar_url._url)
+        )
+        for embed in embed_list:
+            await ctx.send(embed=embed)
+        return
+
+    player_obj = verification_payload['player_obj']
+    clan_obj = verification_payload['clan_obj']
+
+    field_dict_list = []
+    # finding the user for each member in the clan
+    for member_obj in clan_obj.members:
+        field_dict_list.append(discord_responder.find_user_from_tag(
+            member_obj, ctx.guild.members))
+
+    embed_list = discord_responder.embed_message(
+        Embed=discord.Embed,
+        color=discord.Color(client_data.embed_color),
+        icon_url=(ctx.bot.user.avatar_url.BASE +
+                  ctx.bot.user.avatar_url._url),
+        title=f"{clan_obj.name} linked users",
+        bot_prefix=ctx.prefix,
+        bot_user_name=ctx.bot.user.name,
+        thumbnail=player_obj.league_icons,
         field_list=field_dict_list,
         image_url=None,
         author_display_name=ctx.author.display_name,
