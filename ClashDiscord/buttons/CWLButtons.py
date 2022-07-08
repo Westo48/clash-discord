cff99c594d96f9f3da7da1f8f0eb3c42449268c7
Westo48 <westonsouthers19@gmail.com>
2022-07-07 20:15:45 -0500

    CWL Round Scoreboard Button

--- ClashDiscord/buttons/CWLButtons.py
+++ ClashDiscord/buttons/CWLButtons.py
@@ -7,7 +7,8 @@ from data import ClashDiscord_Client_Data as ClientData
 
 from responders.CWLResponder import (
     cwl_info_scoreboard,
-    cwl_scoreboard_group)
+    cwl_scoreboard_group,
+    cwl_scoreboard_round)
 from responders.ClashResponder import (
     cwl_current_round)
 from responders.DiscordResponder import (
@@ -179,6 +180,126 @@ class CWLGroupScoreboardBtn(Button):
             thumbnail=self.clan.badge.small,
             field_list=field_dict_list,
             author=inter.author)
+
+        self.label = btn_name
+
+        # edit the original message with the updated embeds
+        await inter.edit_original_message(
+            embeds=embed_list, view=self.view)
+
+
+class CWLRoundScoreboardBtn(Button):
+    def __init__(
+        self,
+        client_data: ClientData.ClashDiscord_Data,
+        coc_client,
+        clan: Clan,
+        group: ClanWarLeagueGroup,
+        btn_name: str = "Group Scoreboard",
+        btn_style: ButtonStyle = ButtonStyle.primary
+    ):
+        super().__init__(
+            label=btn_name,
+            style=btn_style)
+        self.client_data = client_data
+        self.coc_client = coc_client
+        self.clan = clan
+        self.group = group
+
+    async def callback(
+            self,
+            inter: MessageInteraction):
+
+        await inter.response.defer()
+
+        if self.group is None:
+            embed_description = f"could not find war"
+
+            embed_list = embed_message(
+                icon_url=inter.bot.user.avatar.url,
+                bot_user_name=inter.me.display_name,
+                description=embed_description,
+                author=inter.author)
+
+            await inter.edit_original_message(embeds=embed_list)
+            return
+
+        if self.clan is None:
+            embed_description = f"could not find clan"
+
+            embed_list = embed_message(
+                icon_url=inter.bot.user.avatar.url,
+                bot_user_name=inter.me.display_name,
+                description=embed_description,
+                author=inter.author)
+
+            await inter.edit_original_message(embeds=embed_list)
+            return
+
+        btn_name = self.label
+
+        self.label = f"Please Wait"
+
+        await inter.edit_original_message(view=self.view)
+
+        embed_list = []
+        league_emoji = get_emoji(
+            f"Clan War {self.clan.war_league.name}",
+            inter.client.emojis,
+            self.client_data.emojis)
+        embed_title = f"CWL {league_emoji} {self.clan.war_league.name} Group"
+
+        embed_list.extend(embed_message(
+            icon_url=inter.bot.user.avatar.url,
+            bot_user_name=inter.me.display_name,
+            title=embed_title,
+            description=embed_description,
+            thumbnail=self.clan.badge.small,
+            author=inter.author
+        ))
+
+        round_index = 0
+
+        for cwl_round in self.group.rounds:
+
+            # checking if war has ended
+            war = await self.coc_client.get_league_war(cwl_round[0])
+            if war.state != "warEnded":
+                break
+
+            field_dict_list = await cwl_scoreboard_round(
+                self.group, cwl_round, round_index,
+                inter.client.emojis, self.client_data.emojis,
+                self.coc_client)
+
+            # update round index
+            round_index += 1
+            embed_title = f"**Round: {round_index}**"
+
+            embed_list.extend(embed_message(
+                icon_url=inter.bot.user.avatar.url,
+                bot_user_name=inter.me.display_name,
+                title=embed_title,
+                description=embed_description,
+                field_list=field_dict_list,
+                author=inter.author
+            ))
+
+        if len(embed_list) == 1:
+            field_dict_list = [{
+                "name": f"no rounds have ended",
+                "value": f"please wait till after the first round is over"
+            }]
+
+            embed_list = embed_message(
+                icon_url=inter.bot.user.avatar.url,
+                bot_user_name=inter.me.display_name,
+                title=embed_title,
+                description=embed_description,
+                field_list=field_dict_list,
+                thumbnail=self.clan.badge.small,
+                author=inter.author
+            )
 
         self.label = btn_name
 
