ded9b31d60b189394ae49a46ab55ba663483cdbd
Westo48 <westonsouthers19@gmail.com>
2022-09-21 21:35:41 -0500

    user commands in user cog

--- ClashDiscord/cogs/user.py
+++ ClashDiscord/cogs/user.py
@@ -17,6 +17,105 @@ class User(commands.Cog):
         self.coc_client = coc_client
         self.client_data = client_data
 
+    @commands.user_command(name="profile")
+    async def user_profile(self, inter, user):
+        """
+            show the user's clash profile of linked accounts
+
+            Parameters
+            ----------
+            user (optional): user to run command for
+        """
+        # defer for every command
+        await inter.response.defer()
+
+        # initializing embed default values
+        embed_title = None
+        embed_description = None
+        field_dict_list = []
+
+        db_player_list = db_responder.read_player_list(user.id)
+
+        # user has no claimed players
+        if len(db_player_list) == 0:
+            embed_description = (f"{user.mention} does not have any "
+                                 f"claimed players")
+
+            embed_list = discord_responder.embed_message(
+                icon_url=inter.bot.user.avatar.url,
+                description=embed_description,
+                bot_user_name=inter.me.display_name,
+                author=inter.author
+            )
+
+            await discord_responder.send_embed_list(inter, embed_list)
+            return
+
+        embed_title = f"{user.display_name} Profile"
+        embed_description = f"Player Count: {len(db_player_list)}"
+
+        field_dict_list = await client_user_profile(
+            db_player_list=db_player_list,
+            user=user,
+            discord_emoji_list=inter.client.emojis,
+            client_emoji_list=self.client_data.emojis,
+            coc_client=self.coc_client)
+
+        embed_list = discord_responder.embed_message(
+            icon_url=inter.bot.user.avatar.url,
+            title=embed_title,
+            description=embed_description,
+            bot_user_name=inter.me.display_name,
+            field_list=field_dict_list,
+            author=inter.author)
+
+        await discord_responder.send_embed_list(inter, embed_list)
+
+    @commands.user_command(name="player list")
+    async def user_player_list(self, inter, user):
+        """
+            show the list of players a user has
+
+            Parameters
+            ----------
+        """
+
+        # defer for every command
+        await inter.response.defer()
+
+        # setting user to author if not specified
+        if user is None:
+            user = inter.author
+
+        # initializing embed default values
+        embed_title = None
+        embed_description = None
+        field_dict_list = []
+
+        db_player_list = db_responder.read_player_list(user.id)
+
+        # user has no claimed players
+        if len(db_player_list) == 0:
+            embed_description = (f"{user.mention} does not have any "
+                                 f"claimed players")
+
+            embed_list = discord_responder.embed_message(
+                icon_url=inter.bot.user.avatar.url,
+                description=embed_description,
+                bot_user_name=inter.me.display_name,
+                author=inter.author
+            )
+
+            await discord_responder.send_embed_list(inter, embed_list)
+            return
+
+        message = await client_player_list(
+            db_player_list=db_player_list,
+            user=user,
+            coc_client=self.coc_client)
+
+        await inter.edit_original_message(content=message)
+
     @commands.slash_command()
     async def user(self, inter):
         """
